ca427f6149102da0b3c28d32e18c3635377d9013
Orj <g.jivanphysics@gmail.com>
2023-04-28 01:23:16 +0545

    Project Internal FIles Duplicates Removed

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from detector import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('upload/',views.save_video,name='upload')
-]
